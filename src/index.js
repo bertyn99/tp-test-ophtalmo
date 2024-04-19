840ba3ec9a8423aa914b8e2ed96869d9193f4512
GuillaumeLeGoff <legoff.guill@gmail.com>
2024-04-19 14:42:47 +0200

    Ajouter la prise en charge de Swagger pour la documentation de l'API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import sessionRoutes from './routes/session.js';
+import sessionRoutes from './components/session/session.js';
+import swagger from './swagger.js';
 
 const app = express();
 
 app.use(express.json());
 app.use('/api/sessions', sessionRoutes);
+
+swagger(app);
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
