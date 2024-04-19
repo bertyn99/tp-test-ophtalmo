a710a1a2397b74e96b9c39315e964c4b578ff264
GuillaumeLeGoff <legoff.guill@gmail.com>
2024-04-19 14:47:22 +0200

    Mettre à jour l'import de sessionRoutes dans src/index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import express from 'express';
-import sessionRoutes from './components/session/session.js';
+import sessionRoutes from './components/session/session.router.js';
 import swagger from './swagger.js';
+import loadRoutes from './loaders/routes.js';
 
 const app = express();
 
-app.use(express.json());
-app.use('/api/sessions', sessionRoutes);
-
+loadRoutes(app);
 swagger(app);
 
 app.listen(3000, () => {
